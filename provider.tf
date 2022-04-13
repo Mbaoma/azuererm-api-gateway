3180ff99cbf4e34d701e4cd2c7e56d30259d7a65
Mbaoma <mmaryraphaella@gmail.com>
2022-04-13 10:37:07 +0100

    updated script

---
+++ provider.tf
@@ -0,0 +1,14 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "publisher_name" {
+  type        = string
+  default     = "Mar"
+  description = "publisher name"
+}
+
+variable "publisher_email" {
+  type        = string
+  default     = "company@terraform.io"
+  description = "Publisher email"
+}
+
+variable "sku" {
+  type        = string
+  default     = "Developer_1"
+  description = "SKU"
+}
+
+variable "revision" {
+  type        = string
+  default     = "1"
+  description = "API revision"
+}
+
+variable "display_name" {
+  type        = string
+  default     = "Test API"
+  description = "API display name"
+}
+
+variable "path" {
+  type        = string
+  default     = ""
+  description = "Endpoint path"
+}
+
+variable "protocols" {
+  type        = list
+  default     = ["https", "http"]
+  description = "API security protocols"
+}
+
+variable "service_url" {
+  type        = string
+  default     = "http://conferenceapi.azurewebsites.net"
+  description = "URL"
+}
